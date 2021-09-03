b0670049a8b0d798fd2bae0f83f7c903ea91fbdf
devandtravel <devandtravel@gmail.com>
2021-09-03 16:17:09 +0300

    initial commit from my vite redux query template

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import App from './App'
+import './index.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
